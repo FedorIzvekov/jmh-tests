9eda2c1895b65d9329b1c6b5f5349421f11f59ae
FedorIzvekov <fedor.izvekov@gmail.com>
2018-05-06 09:15:32 +0300

    create project jmh-tests

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project jmh-tests
+This project features JMH tests.
+
+## Building, Running and Checking
+
+1. Navigate to the project directory:
+```
+cd <projects_directory>/jmh-tests
+```
+
+2. Build the project:
+```
+mvn clean install
+```
+
+3. Run the server:
+```
+java -jar ./target/jmh-tests-1.0.0-SNAPSHOT.jar
+```
---
+++ src/main/java/com/fedorizvekov/jmh/tests/BenchmarkRunner.java
@@ -0,0 +1,27 @@
+package com.fedorizvekov.jmh.tests;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+public class BenchmarkRunner {
+
+    public static void main(String[] args) throws RunnerException {
+
+        Options options = new OptionsBuilder()
+                .include(StringConcatenation.class.getSimpleName())
+                .warmupIterations(10)
+                .warmupTime(TimeValue.seconds(1))
+                .measurementIterations(10)
+                .measurementTime(TimeValue.seconds(10))
+                .forks(2)
+                .shouldFailOnError(true)
+                .shouldDoGC(true)
+                .build();
+
+        new Runner(options).run();
+    }
+
+}
