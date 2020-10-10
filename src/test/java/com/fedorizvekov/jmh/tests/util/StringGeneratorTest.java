1dc274e5bfbb77937b43673635a3c45527bd76d1
FedorIzvekov <fedor.izvekov@gmail.com>
2020-10-10 16:12:26 +0300

    updated dependency

--- src/test/java/com/fedorizvekov/jmh/tests/util/StringGeneratorTest.java
+++ src/test/java/com/fedorizvekov/jmh/tests/util/StringGeneratorTest.java
@@ -2,12 +2,12 @@ package com.fedorizvekov.jmh.tests.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class StringGeneratorTest {
+class StringGeneratorTest {
 
     @Test
-    public void should_generate_string_of_10_length() {
+    void should_generate_string_of_10_length() {
         String result = StringGenerator.generate(10);
         assertThat(result).hasSize(10);
         assertThat(result).matches("[A-Za-z]+");
@@ -15,7 +15,7 @@ public class StringGeneratorTest {
 
 
     @Test
-    public void should_generate_string_of_100_length() {
+    void should_generate_string_of_100_length() {
         String result = StringGenerator.generate(100);
         assertThat(result).hasSize(100);
         assertThat(result).matches("[A-Za-z]+");
